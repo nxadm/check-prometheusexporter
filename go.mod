8dae1dd153217febfb3649ba6100509159501137
Claudio Ramirez <pub.claudio@gmail.com>
2020-03-31 16:12:49 +0200

    fix nil pointer

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/nxadm/check-prometheusexporter
 
 go 1.14
+
+require github.com/stretchr/testify v1.5.1
--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 const appname = "check-prometheusexporter"
-const version = "0.2.3"
+const version = "0.3.0"
 const author = "Claudio Ramirez <pub.claudio@gmail.com>"
 const repo = "https://github.com/nxadm/" + appname + ".git"
 
@@ -30,44 +30,45 @@ type Config struct {
 
 func main() {
 	/* Read the CLI parameters */
-	config := Config{}
-	config.readParams()
+	cfg := handleCLI()
+	fmt.Printf("%#v\n", cfg)
+
 
 	/* Query the probe */
-	answer, err := getMetrics(&config)
-	// fmt.Printf("ANSWER: %+v\n", answer) //debug
+	answer, err := getMetrics(&cfg)
+	fmt.Printf("ANSWER: %#v\n%#v\n", answer, err) //debug
 	if err != nil {
-		if answer.TimedOut {
-			fmt.Printf("[CRITICAL] Timeout (%d) reached: %v\n", config.TimeoutSec, err)
+		if answer != nil && answer.TimedOut {
+			fmt.Printf("[CRITICAL] Timeout (%d) reached: %v\n", cfg.TimeoutSec, err)
 			os.Exit(CRITICAL)
 		} else {
 			fmt.Printf("[UNKNOWN] Can not decode the server's answer: %v\n", err)
 			os.Exit(UNKNOWN)
 		}
 	}
-
-	if answer.Success == false {
-		fmt.Println("[CRITICAL] The service reports a failure.")
-		os.Exit(CRITICAL)
-
-	}
-
-	switch {
-	case answer.Duration >= float64(config.CriticalSec):
-		fmt.Printf(
-			"[CRITICAL] Check duration (%f) was higher than critical threshold (%d).\n",
-			answer.Duration, config.CriticalSec)
-		os.Exit(CRITICAL)
-	case answer.Duration >= float64(config.WarningSec):
-		fmt.Printf(
-			"[WARNING] Check duration (%f) was higher than warning threshold (%d).\n",
-			answer.Duration, config.WarningSec)
-		os.Exit(WARNING)
-	default:
-		fmt.Printf(
-			"[OK] Check duration (%f) lower than thresholds (critical: %d, warning: %d).\n",
-			answer.Duration, config.CriticalSec, config.WarningSec)
-		os.Exit(OK)
-	}
+	//
+	//if answer.Success == false {
+	//	fmt.Println("[CRITICAL] The service reports a failure.")
+	//	os.Exit(CRITICAL)
+	//
+	//}
+	//
+	//switch {
+	//case answer.Duration >= float64(cfg.CriticalSec):
+	//	fmt.Printf(
+	//		"[CRITICAL] Check duration (%f) was higher than critical threshold (%d).\n",
+	//		answer.Duration, cfg.CriticalSec)
+	//	os.Exit(CRITICAL)
+	//case answer.Duration >= float64(cfg.WarningSec):
+	//	fmt.Printf(
+	//		"[WARNING] Check duration (%f) was higher than warning threshold (%d).\n",
+	//		answer.Duration, cfg.WarningSec)
+	//	os.Exit(WARNING)
+	//default:
+	//	fmt.Printf(
+	//		"[OK] Check duration (%f) lower than thresholds (critical: %d, warning: %d).\n",
+	//		answer.Duration, cfg.CriticalSec, cfg.WarningSec)
+	//	os.Exit(OK)
+	//}
 
 }
